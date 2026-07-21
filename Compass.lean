0c1c4d60517c23545700db1d79c536aad34d6a3f
Weiyi Wang <wwylele@gmail.com>
2026-07-20 22:16:56 -0400

    mk all

--- Compass.lean
+++ Compass.lean
@@ -4,4 +4,5 @@ public import Compass.CommonConstruction
 public import Compass.ConstructibleNumber
 public import Compass.ConstructiblePoint
 public import Compass.Equivalence
+public import Compass.Polygon
 public import Compass.Wiedijk8
